8f9e69c53f683a9b4a589b3ab46861a53b72f800
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-10-16 21:45:37 +0500

    updated PORT for render

--- main.py
+++ main.py
@@ -366,5 +366,5 @@ async def create_new_session():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
